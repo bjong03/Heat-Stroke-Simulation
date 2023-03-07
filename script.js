fcbb7b4276933ea4bc33fbeda553a34d45d2def2
bjong03 <87583080+bjong03@users.noreply.github.com>
2023-03-06 17:01:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-TESTER = document.getElementById('tester');
+const slider1 = document.getElementById("slider1");
+const slider1Value = document.getElementById("slider1Value");
+const slider2 = document.getElementById("slider2");
+const slider2Value = document.getElementById("slider2Value");
+const slider3 = document.getElementById("slider3");
+const slider3Value = document.getElementById("slider3Value");
+const sumDisplay = document.getElementById("sum");
 
-Plotly.plot( TESTER, [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] }], { 
-    margin: { t: 0 } }, {showSendToCloud:true} );
+function updateSum() {
+  const sum = Number(slider1.value) + Number(slider2.value) + Number(slider3.value);
+  sumDisplay.textContent = "Sum: " + sum;
+}
 
-/* Current Plotly.js version */
-console.log( Plotly.BUILD );
+function updateSliderValue(slider, valueDisplay) {
+  valueDisplay.textContent = slider.value;
+}
+
+slider1.addEventListener("input", () => {
+  updateSliderValue(slider1, slider1Value);
+  updateSum();
+});
+slider2.addEventListener("input", () => {
+  updateSliderValue(slider2, slider2Value);
+  updateSum();
+});
+slider3.addEventListener("input", () => {
+  updateSliderValue(slider3, slider3Value);
+  updateSum();
+});
+
+updateSliderValue(slider1, slider1Value);
+updateSliderValue(slider2, slider2Value);
+updateSliderValue(slider3, slider3Value);
+updateSum(); // Initialize sum display
